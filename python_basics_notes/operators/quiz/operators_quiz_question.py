61b848d9abcd61d00db69c33d37741a5c34ba978
hariom2311 <hariomsuryawanshi68258@gmail.com>
2023-06-11 08:20:31 +0530

    updated structure of files

---
+++ python_basics_notes/operators/quiz/operators_quiz_question.py
@@ -0,0 +1,104 @@
+
+# Quiz Questins
+
+"""Q1. What is the value of the variable a after executing the following code?"""
+a = 5
+a += 2
+
+"""Options
+a) 4
+b) 7
+c) 2
+d) 5
+"""
+
+"""Q2. Which data type is used to represent a sequence of characters in Python?"""
+
+"""Options
+a) int
+b) float
+c) str
+d) bool
+"""
+
+"""Q3. What is the result of the following expression?"""
+3 * 2 + 4 / 2 - 1
+
+"""Options
+a) 4
+b) 5
+c) 6
+d) 7
+"""
+
+"""Q4. What is the output of the following code?"""
+name = "John"
+print("Hello, " + name + "!")
+
+"""Options
+a) Hello, John!
+b) Hello, name!
+c) Hello, "John"!
+d) Hello, !
+"""
+
+"""Q5. Which of the following variable names is invalid in Python?"""
+
+"""Options
+a) my_name
+b) 123name
+c) _name
+d) NAME
+"""
+
+"""Q6. What is the value of the variable result after executing the following code?"""
+x = 10
+y = 5
+result = x // y
+
+"""Options
+a) 0
+b) 2
+c) 2.0
+d) None
+"""
+
+"""Q7. Which operator is used for exponentiation in Python?"""
+
+"""Options
+a) +
+b) -
+c) *
+d) **
+"""
+
+"""Q8. What is the output of the following code?"""
+x = 4
+y = 2
+print(x % y)
+
+"""Options
+a) 2
+b) 1
+c) 0
+d) 4
+"""
+
+"""Q9. What is the result of the following expression?"""
+print(True and False)
+
+"""Options
+a) True
+b) False
+c) Error
+d) None
+"""
+
+"""Which function is used to convert a string to an integer in Python?"""
+
+"""Options
+a) str()
+b) int()
+c) float()
+d) bool()
+"""
